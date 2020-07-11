39845c44bc87615c90fa5749d7da506de876048f
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2020-07-11 18:39:54 +0200

    add elements in main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('form');
+const inputAdd = document.querySelector('input');
+const inputSearch = document.querySelector('input.search');
+const sectionTasks = document.querySelector('section.toDoList');
