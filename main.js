ad29e3508e38a35b868024e66148e5b5a7555a16
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2020-07-11 18:41:57 +0200

    create function addTask and function delate tasks

--- main.js
+++ main.js
@@ -2,3 +2,35 @@ const form = document.querySelector('form');
 const inputAdd = document.querySelector('input');
 const inputSearch = document.querySelector('input.search');
 const sectionTasks = document.querySelector('section.toDoList');
+
+const addTask = (e) => {
+    e.preventDefault();
+    if (inputAdd.value !== "") {
+        const titleTask = inputAdd.value;
+        // add div to section
+        const taskList = document.createElement('div');
+        taskList.classList.add('taskList')
+        sectionTasks.appendChild(taskList)
+        // add div-task to div-taskList
+        const task = document.createElement('div');
+        task.classList.add('task')
+        task.innerHTML = `${titleTask}`;
+        taskList.appendChild(task);
+        // add button to taskList
+        const del = document.createElement('button');
+        del.classList.add("delate");
+        del.innerHTML = `Usuń zadanie`
+        taskList.appendChild(del);
+        // add function removeTask
+        del.addEventListener('click', e => {
+            e.target.parentElement.remove()
+        })
+
+        inputAdd.value = "";
+    } else {
+        alert('Nie wpisałeś żadnego zadania!')
+    }
+
+}
+
+form.addEventListener('submit', addTask);
