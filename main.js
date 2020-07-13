b06f1b200fed44b9e2efefe6562db8d93e5234b2
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2020-07-13 14:54:28 +0200

    add array, fix function addTask, taskTemplate, deleteTask and searchTask

--- main.js
+++ main.js
@@ -3,38 +3,58 @@ const inputAdd = document.querySelector('input');
 const inputSearch = document.querySelector('input.search');
 const sectionTasks = document.querySelector('section.toDoList');
 
+let tasks = [];
+
 const addTask = (e) => {
     e.preventDefault();
-    if (inputAdd.value !== "") {
-        const titleTask = inputAdd.value;
-        // add div to section
-        const taskList = document.createElement('div');
-        taskList.classList.add('taskList')
-        sectionTasks.appendChild(taskList)
-        // add div-task to div-taskList
-        const task = document.createElement('div');
-        task.classList.add('task')
-        task.innerHTML = `${titleTask}`;
-        taskList.appendChild(task);
-        // add button to taskList
-        const del = document.createElement('button');
-        del.classList.add("delate");
-        del.innerHTML = `Usuń zadanie`
-        taskList.appendChild(del);
-        // add function removeTask
-        del.addEventListener('click', e => {
-            e.target.parentElement.remove()
-        })
+    if (!inputAdd.value) return alert('Nie wpisałeś żadnego zadania!')
 
-        inputAdd.value = "";
-    } else {
-        alert('Nie wpisałeś żadnego zadania!')
-    }
-
+    tasks.push(inputAdd.value);
+    form.reset();
+    showTasks();
 }
 
-const searchTask = (e) => {
-    const searchText = e.target.value.toLowerCase();
+const showTasks = (filtered) => {
+    sectionTasks.innerHTML = '';
+    const this_tasks = filtered || tasks;
+    this_tasks.forEach((title, index) => {
+        taskTemplate(title, index);
+
+    });
+}
+
+const taskTemplate = (title, index) => {
+    // add div to section
+    const taskList = document.createElement('div');
+    taskList.classList.add('taskList')
+    sectionTasks.appendChild(taskList)
+    // add div-task to div-taskList
+    const task = document.createElement('div');
+    task.classList.add('task')
+    task.innerHTML = title;
+    taskList.appendChild(task);
+    // add button to taskList
+    const del = document.createElement('button');
+    del.classList.add("delate");
+    del.innerHTML = `Usuń zadanie`
+    taskList.appendChild(del);
+    // add function removeTask
+    del.addEventListener('click', () => {
+        deleteTask(index);
+    });
+}
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    showTasks();
+}
+
+const searchTask = () => {
+    const filteredTasks = tasks.filter((title) => {
+        return title.toLowerCase().includes(inputSearch.value.toLowerCase());
+    });
+
+    showTasks(filteredTasks);
 }
 
 form.addEventListener('submit', addTask);
